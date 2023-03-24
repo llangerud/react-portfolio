fd55b25fae8c8c01fc9daf717615b52cc0a5f590
lileee <112982735+lileeeeeee@users.noreply.github.com>
2023-03-23 23:51:43 -0500

    updated icons and links in bottom navbar, added portfolio links and project images

--- src/components/BottomNav.js
+++ src/components/BottomNav.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { solid, regular, brands, icon } from '@fortawesome/fontawesome-svg-core/import.macro'
 
 
 
 function BottomNav () {
     return <Navbar fixed="bottom" bg='primary'>
     <Container>
-        <Navbar.Brand href="#">BottomNav</Navbar.Brand>
+        <Navbar.Brand href="https://linkedin.com/in/lily-langerud"><i class="fa-brands fa-linkedin"></i></Navbar.Brand>
+        <Navbar.Brand href="https://github.com/llangerud"><i class="fa-brands fa-github"></i></Navbar.Brand>
+        <Navbar.Brand href="https://www.hackerrank.com/lily_langerud"><i class="fa-brands fa-hackerrank"></i></Navbar.Brand>
       </Container>
       </Navbar>
 }
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -3,6 +3,10 @@ import Container from 'react-bootstrap/Container'
 import Card from 'react-bootstrap/Card';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+
+
+
+
 export default function Portfolio() {
   return (
     <Container>
@@ -10,68 +14,79 @@ export default function Portfolio() {
   <Row>
   <Col sm>
    <Card style = {{ width: '18rem' }} className="text-center m-3" text='dark'>
-      <Card.Img variant="top" src='/images/dogone.jpg'/>
+   <Card.Header>Did I Need That</Card.Header>
+   <Card.Link href="https://did-i-need-that.herokuapp.com/">Application</Card.Link>
+        <Card.Link href="https://github.com/llangerud/did-i-need-that">GitHub </Card.Link>
+      <Card.Img variant="top" src='/images/dint.jpg'/>
       <Card.Body>
-        <Card.Title>About</Card.Title>
-        <Card.Text>
-          Lily lives in Minneapolis with her partner and two dogs. 
+      <Card.Text>
+          Full-stack application for tracking purchases using SQL, express and nodemailer. 
         </Card.Text>
         </Card.Body>
     </Card>
     </Col>
     <Col sm>
     <Card style = {{ width: '18rem' }} className="text-center m-3" text='dark'>
-      <Card.Img variant="top" src='/images/dogone.jpg'/>
+    <Card.Header>Cryptoscope</Card.Header>
+    <Card.Link href="https://llangerud.github.io/cryptoscope/">Application</Card.Link>
+        <Card.Link href="https://github.com/llangerud/cryptoscope">GitHub</Card.Link>
+      <Card.Img variant="top" src='/images/cryptoscopeimage.jpg'/>
       <Card.Body>
-        <Card.Title>About</Card.Title>
         <Card.Text>
-          Lily lives in Minneapolis with her partner and two dogs. 
+          Front-end application offering realtime crypto data.
         </Card.Text>
         </Card.Body>
     </Card>
     </Col>
     <Col sm>
     <Card style = {{ width: '18rem' }} className="text-center m-3" text='dark'>
-      <Card.Img variant="top" src='/images/dogone.jpg'/>
+    <Card.Header>Just Another Text Editor</Card.Header>
+    <Card.Link href="https://text-editor87756.herokuapp.com/">Application</Card.Link>
+        <Card.Link href="https://github.com/llangerud/text-editor">GitHub </Card.Link>
+      <Card.Img variant="top" src='/images/jate.jpg'/>
       <Card.Body>
-        <Card.Title>About</Card.Title>
         <Card.Text>
-          Lily lives in Minneapolis with her partner and two dogs. 
-        </Card.Text>
+        A Progressive Web Application (PWA) built with Node, Express, CodeMirror, IndexedDB and webpack.        </Card.Text>
         </Card.Body>
     </Card>
     </Col>
     </Row>
     <Row>
       <Col sm>
-    <Card style = {{ width: '18rem' }} className="text-center m-3" text='dark'>
-      <Card.Img variant="top" src='/images/dogone.jpg'/>
+      <Card style = {{ width: '18rem' }} className="text-center m-3" text='dark'>
+    <Card.Header>Employee Tracker</Card.Header>
+    <Card.Link href="#">Application</Card.Link>
+        <Card.Link href="#">GitHub </Card.Link>
+      <Card.Img variant="top" src='/images/employeetracker.jpg'/>
       <Card.Body>
-        <Card.Title>About</Card.Title>
         <Card.Text>
-          Lily lives in Minneapolis with her partner and two dogs. 
+        Command-line application using Node, inquirer and mysql to create a searchable employee database.
         </Card.Text>
         </Card.Body>
     </Card>
     </Col>
     <Col sm>
     <Card style = {{ width: '18rem' }} className="text-center m-3" text='dark'>
-      <Card.Img variant="top" src='/images/dogone.jpg'/>
+    <Card.Header>Employee Tracker</Card.Header>
+    <Card.Link href="#">Application</Card.Link>
+        <Card.Link href="#">GitHub </Card.Link>
+      <Card.Img variant="top" src='/images/employeetracker.jpg'/>
       <Card.Body>
-        <Card.Title>About</Card.Title>
         <Card.Text>
-          Lily lives in Minneapolis with her partner and two dogs. 
+        Command-line application using Node, inquirer and mysql to create a searchable employee database.
         </Card.Text>
         </Card.Body>
     </Card>
     </Col>
     <Col sm>
     <Card style = {{ width: '18rem' }} className="text-center m-3" text='dark'>
-      <Card.Img variant="top" src='/images/dogone.jpg'/>
+    <Card.Header>Employee Tracker</Card.Header>
+    <Card.Link href="#">Application</Card.Link>
+        <Card.Link href="#">GitHub </Card.Link>
+      <Card.Img variant="top" src='/images/employeetracker.jpg'/>
       <Card.Body>
-        <Card.Title>About</Card.Title>
         <Card.Text>
-          Lily lives in Minneapolis with her partner and two dogs. 
+        Command-line application using Node, inquirer and mysql to create a searchable employee database.
         </Card.Text>
         </Card.Body>
     </Card>
